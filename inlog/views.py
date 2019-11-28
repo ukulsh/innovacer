8945cac1648a7c8ce1cc2db609fbe9dc921db244
Utkarsh <utkarshkulshrestha98@gmail.com>
2019-11-28 22:01:03 +0530

    integrated mail sending feature

--- inlog/views.py
+++ inlog/views.py
@@ -12,9 +12,32 @@ def contact_view(request):
         visitor.email= request.POST.get("email")
         visitor.host = request.POST.get("message")
         visitor.save()
-        print(visitor.name,visitor.email,visitor.host)
+        name = visitor.name
+        email = visitor.email
+        host = visitor.host
+        inTime = visitor.inTime
+        
+        content = '\nVisitor Details: \n'
+        content += 'Name: '+str(name)+'\n'
+        content += 'Email: '+str(email)+'\n'
+        content += 'Host: '+str(host)+ '\n'
+        content += 'Checkin Time: '+str(inTime)+'\n'
+        
+        mail = SMTP('smtp.gmail.com',587)
+        mail.ehlo()
+
+        # email = input("Enter a valid email to send mails from:")
+        # password = input("Enter password:")
+
+        mail.starttls()
+        mail.login('dustingdown@gmail.com','Utkarsh1@')
+        mail.sendmail('dustingdown@gmail.com',email,content)
+
+        mail.close()
+
+        # print(visitor.name,visitor.email,visitor.host)
         return render(request,"home.html")
-        content = 'example email stuff here'
+        
     else:
         return render(request,"home.html")
 
@@ -23,6 +46,31 @@ def contact_view2(request):
     if request.method == "POST":
         
         n = request.POST.get("name")
+        details = Visitor.objects.get(name=n)
+        print(details)
+        mail = SMTP('smtp.gmail.com',587)
+        mail.ehlo()
+        
+        # email = input("Enter a valid email to send mails from:")
+        # password = input("Enter password:")
+
+        name = details.name
+        email = details.email
+        host = details.host
+        inTime = details.inTime
+
+        content = '\nVisitor Details: \n'
+        content += 'Name: '+str(name)+'\n'
+        content += 'Email: '+str(email)+'\n'
+        content += 'Host: '+str(host)+ '\n'
+        content += 'Checkin Time: '+str(inTime)+'\n'
+        
+
+        mail.starttls()
+        mail.login('dustingdown@gmail.com','Utkarsh1@')
+        mail.sendmail('dustingdown@gmail.com',email,content)
+
+        mail.close()
         Visitor.objects.get(name=n).delete()
         # print(visitor.name,visitor.email,visitor.host,file=sys.stderr)
         return render(request,"home.html")
@@ -39,16 +87,6 @@ def index2_call(request):
 def home(request):
     return render(request, "home.html")
 
-def chick(request):
-    
-    all_objects= Visitor.objects.all()
-    
-    context = {
-            'all_objects': all_objects
-        }
-    print(context)    
-    return render(request,'datab.html',context)
-    # return render(request, "datab.html")
 
 
 
